8aea3d5e54d1e6eb44ea1ccd2e039b90a71e500c
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-11-22 23:36:54 -0500

    Built the Nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
 
 function Nav() {
-  return <div></div>;
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://cdn.freebiesupply.com/logos/large/2x/netflix-2-logo-png-transparent.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
 }
 
 export default Nav;
